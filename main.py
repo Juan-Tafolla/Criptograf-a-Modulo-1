da823384cfa59cb5ef5b39a767c833889032405d
Juan-Tafolla <juantafolla2001@gmail.com>
2022-09-14 18:42:44 -0500

    Set-Up the main file

---
+++ main.py
@@ -0,0 +1,21 @@
+#####################################################################
+#### Primer proyecto del modulo 1 de la asignatura de criptografia
+# 
+# Integrantes del equipo:
+#       - Avalos Villarruel Isaac Naason
+#       - Mora Carbajal Erick Fernando
+#       - Tafolla Fraga Juan Pablo
+#
+#####################################################################
+
+import HillCipher as hill
+import PlayfairCipher as plfr
+import OTPCipher as otp
+
+class Main:
+    print('Hello World')
+
+
+if __name__ == '__main__':
+    app = Main()
+    app.start()
